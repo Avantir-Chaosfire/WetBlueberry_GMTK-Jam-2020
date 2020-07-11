91a5b66e812c0c18f692c967036c21d076078b49
Eddie Marchioni <emarchio@sfu.ca>
2020-07-11 12:32:47 -0700

    Incorporates enemy textures and makes their corpses linger

--- scene/object/enemy/Body.gd
+++ scene/object/enemy/Body.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 onready var player = get_node("../../Player/Body")
+onready var animationPlayer = get_node("AnimationPlayer")
+onready var sprite = get_node("Sprite")
 
 const MaxMovementSpeed = 190
 const MaxFarMovementSpeed = 380
@@ -11,49 +13,69 @@ const Friction = 330
 const FastSpeedDistance = 501
 
 var velocity = Vector2()
+var isMoving = false
+var dead = false
+
+func _ready():
+	animationPlayer.play("Idle")
 
 func _physics_process(delta):
-	var targetPosition = get_parent().to_local(player.getGlobalPosition())
-	var targetVector = targetPosition - position
-	
-	var detectionRange = DetectionRange
-	if player.isMourning:
-		detectionRange = MourningDetectionRange
-	
-	if targetVector.length() < detectionRange:
-		var maxMovementSpeed = MaxMovementSpeed
-		if targetVector.length() > FastSpeedDistance:
-			maxMovementSpeed = MaxFarMovementSpeed
-		var acceleration = targetVector.normalized() * Acceleration * delta
-		if (velocity + acceleration).length() < maxMovementSpeed:
-			velocity += acceleration
-		elif velocity.length() < maxMovementSpeed:
-			velocity += acceleration
-			velocity = velocity.normalized() * min(velocity.length(), maxMovementSpeed)
+	if not dead:
+		var targetPosition = get_parent().to_local(player.getGlobalPosition())
+		var targetVector = targetPosition - position
+		
+		var detectionRange = DetectionRange
+		if player.isMourning:
+			detectionRange = MourningDetectionRange
+		
+		if targetVector.length() < detectionRange:
+			if not isMoving:
+				isMoving = true
+				animationPlayer.play("Walking")
+			if targetVector.x < 0:
+				sprite.scale.x = -0.5
+			elif targetVector.x > 0:
+				sprite.scale.x = 0.5
+			var maxMovementSpeed = MaxMovementSpeed
+			if targetVector.length() > FastSpeedDistance:
+				maxMovementSpeed = MaxFarMovementSpeed
+			var acceleration = targetVector.normalized() * Acceleration * delta
+			if (velocity + acceleration).length() < maxMovementSpeed:
+				velocity += acceleration
+			elif velocity.length() < maxMovementSpeed:
+				velocity += acceleration
+				velocity = velocity.normalized() * min(velocity.length(), maxMovementSpeed)
+			else:
+				var parallelAcceleration = acceleration.project(velocity)
+				if (velocity + parallelAcceleration).length() < velocity.length():
+					velocity += parallelAcceleration
+				velocity += (acceleration - parallelAcceleration)
 		else:
-			var parallelAcceleration = acceleration.project(velocity)
-			if (velocity + parallelAcceleration).length() < velocity.length():
-				velocity += parallelAcceleration
-			velocity += (acceleration - parallelAcceleration)
-	var originalPosition = Vector2(position)
-	var slidingVelocity = move_and_slide(velocity)
-	if slidingVelocity.length() < velocity.length():
-		var newPosition = Vector2(position)
-		position = originalPosition
-		var collisionInfo = move_and_collide(velocity * delta)
-		if collisionInfo:
-			collisionInfo.collider.Hit(self, collisionInfo.normal)
-		position = newPosition
-	velocity = velocity.normalized() * max(0, velocity.length() - (Friction * delta))
+			if isMoving:
+				isMoving = false
+				animationPlayer.play("Idle")
+		var originalPosition = Vector2(position)
+		var slidingVelocity = move_and_slide(velocity)
+		if slidingVelocity.length() < velocity.length():
+			var newPosition = Vector2(position)
+			position = originalPosition
+			var collisionInfo = move_and_collide(velocity * delta)
+			if collisionInfo:
+				collisionInfo.collider.Hit(self, collisionInfo.normal)
+			position = newPosition
+		velocity = velocity.normalized() * max(0, velocity.length() - (Friction * delta))
 
 func _on_DamageArea_body_entered(body):
-	body.Damage()
+	if not dead:
+		body.Damage()
 
 func getGlobalPosition():
 	return to_global(Vector2())
 
 func Damage():
-	get_parent().unload()
+	dead = true
+	animationPlayer.play("Dead")
+	collision_layer = 0
 
 func Hit(_body, _normal):
 	pass
--- scene/object/enemy/Enemy.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func unload():
-	queue_free()
-	get_parent().remove_child(self)
